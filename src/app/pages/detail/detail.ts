5e452a8daceb04fddd0a08f43fb421435ebd09ca
Vince Navarro <vincent@vincentnavarro.com>
2026-06-24 21:44:29 -0700

    feat: build out detail page with stats, types, and back button

--- src/app/pages/detail/detail.ts
+++ src/app/pages/detail/detail.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { Pokemon, AppState } from '../../models/pokemon.model';
 import { loadPokemonDetail } from '../../store/pokemon.actions';
-import { selectSelectedPokemon } from '../../store/pokemon.selectors';
+import { selectSelectedPokemon, selectLoading } from '../../store/pokemon.selectors';
 
 @Component({
   selector: 'app-detail',
@@ -16,15 +16,22 @@ import { selectSelectedPokemon } from '../../store/pokemon.selectors';
 })
 export class DetailComponent implements OnInit {
   pokemon$!: Observable<Pokemon | null>;
+  loading$!: Observable<boolean>;
 
   constructor(
     private store: Store<AppState>,
     private route: ActivatedRoute,
+    private router: Router,
   ) {}
 
   ngOnInit(): void {
     this.pokemon$ = this.store.select(selectSelectedPokemon);
+    this.loading$ = this.store.select(selectLoading);
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.store.dispatch(loadPokemonDetail({ id }));
+  }
+
+  goBack(): void {
+    this.router.navigate(['/']);
   }
 }
